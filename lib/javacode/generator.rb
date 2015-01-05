5e1f61a3e431c4b2fabb7b897346d7661f5daf17
felit <nofelit@126.com>
2015-01-05 10:43:45 +0800

    init

---
+++ lib/javacode/generator.rb
@@ -0,0 +1,7 @@
+require "javacode/generator/version"
+
+module Javacode
+  module Generator
+    # Your code goes here...
+  end
+end
---
+++ lib/javacode/generator/version.rb
@@ -0,0 +1,5 @@
+module Javacode
+  module Generator
+    VERSION = "0.0.1"
+  end
+end
